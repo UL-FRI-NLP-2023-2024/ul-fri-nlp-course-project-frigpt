8dbce127f9e12a196a6b66e8eb4d84adf5fd442b
Andraz Zrimsek <androzrim@gmail.com>
2024-04-16 11:46:41 +0200

    Added initial code for data processing and sentence retrieval

---
+++ source/utils/data.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+import re
+import os
+
+def join_lines(lines):
+    """
+    Joins lines of a play that are split across multiple lines with newline characters
+    
+    """
+    joined_lines = []
+    current_line = ''
+    for line in lines:
+        if line == '\n':
+            joined_lines.append(current_line)
+            current_line = ''
+            continue
+        current_line += line[:-1] + ' '
+
+    return joined_lines
+
+
+def remove_stage_directions(lines):
+    """
+    Removes stage directions from a line of text
+    """
+    return [re.sub(r'\s\[.*?\]\s', '', line) for line in lines]
+    
+
+def extract_lines_from_play(filepath):
+    """
+    Extracts the lines from a play for each character and returns them in a pandas array
+    """
+    # Read
+    with open(filepath, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+
+    # Clean up text
+    lines = join_lines(lines)
+    lines = remove_stage_directions(lines)
+
+
+    character_lines = {}
+    line_number = 0
+
+    for line in lines:
+        if line.startswith('ACT') or line.startswith('SCENE') or line.startswith(' ACT') or line.startswith(' SCENE') or line.startswith('THE END'):
+            continue
+        if line.startswith('['):
+            continue
+        # Use a regular expression to match the pattern of a character's name at the start of a line
+        match = re.match(r'^((?:MS|MR|MRS\.\s)?[A-Z\s\.]+)\.\s+(.*)', line)
+
+        # If a match is found, store the character's name and the rest of the line in the dictionary
+        if match:
+            character = match.group(1).strip()
+            text = match.group(2).strip()
+
+            if character not in character_lines:
+                character_lines[character] = []
+
+            character_lines[character].append((text, line_number))
+            line_number += 1
+
+    # Convert the dictionary to a pandas DataFrame for easier data manipulation
+    df = pd.DataFrame([(k, v, i) for k, vs in character_lines.items() for v, i in vs], columns=['Character', 'Line', 'Line Number'])
+    return df
+
+if __name__ == '__main__':
+    lines = extract_lines_from_play('data/ideal_husband.txt')
+    lines.to_csv('data/ideal_husband_lines.csv', index=False)
---
+++ source/utils/retrieval.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import numpy as np
+import torch
+from data import extract_lines_from_play
+
+from sentence_transformers import SentenceTransformer
+
+def get_device():
+    """
+    Returns the appropriate device for the current environment
+    """
+    return torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = SentenceTransformer("all-MiniLM-L6-v2", device=get_device())
+
+class Book:
+    def __init__(self, filepath):
+        """
+        Initializes a Book object
+        """
+        lines = extract_lines_from_play(filepath)
+        self.lines = lines
+        self.characters = self.get_characters()
+        self.embeddings = []
+
+        for character in self.characters:
+            character_lines = self.lines[self.lines["Character"] == character]
+            character_line_ids = character_lines["Line Number"].values
+            character_lines_with_context = []
+            for line_number in character_line_ids:
+                context_lines = self.lines[(self.lines["Line Number"] >= max(line_number - 1, 0)) & (self.lines["Line Number"] <= line_number)]
+                character_lines_with_context.append(" ".join(context_lines["Line"].values))
+
+            character_embeddings = generate_embeddings(character_lines_with_context)
+            for i, embedding in enumerate(character_embeddings):
+                self.embeddings.append({"Character": character, "Line": character_lines.iloc[i]["Line"], "Embedding": embedding})
+
+        self.lines = pd.DataFrame(self.embeddings)
+
+    def get_characters(self):
+        """
+        Returns the characters in the book
+        """
+        return self.lines["Character"].unique()
+
+    def get_character_lines(self, character):
+        """
+        Returns the lines spoken by a character
+        """
+        return self.lines[self.lines["Character"] == character]["Line"].values
+
+    def get_best_sentences(self, sentence, character, k=5):
+        """
+        Returns the k most similar lines to the given sentence
+        """
+        character_lines = self.lines[self.lines["Character"] == character]
+        character_sentences = character_lines["Line"].values
+        character_embeddings = character_lines["Embedding"].values
+
+        query_embedding = generate_embeddings([sentence])[0]
+
+        similarities = [get_embedding_similarity(query_embedding, embedding) for embedding in character_embeddings]
+
+        indices = np.argsort(similarities)[::-1][:k]
+        return [character_sentences[i] for i in indices]
+
+def character_lines(character, lines):
+    """
+    Returns the lines of text spoken by a character
+    """
+    ch_lines = lines[lines["Character"] == character]
+    return ch_lines["Line"].values
+
+def generate_embeddings(sentences):
+    """
+    Generates an embedding for a given text
+    """
+    # Load the model
+    embeddings = model.encode(sentences)
+    return embeddings
+
+def get_embedding_similarity(embedding1, embedding2):
+    """
+    Returns the cosine similarity between two embeddings
+    """
+    return np.dot(embedding1, embedding2) / (np.linalg.norm(embedding1) * np.linalg.norm(embedding2))
+    
+if __name__ == '__main__':
+    book = Book("data/hamlet.txt")
+    sentences = book.get_best_sentences("What do you think about Ophelia?", "HAMLET")
+    for i, sentence in enumerate(sentences):
+        print(f"{i + 1}: {sentence}")
