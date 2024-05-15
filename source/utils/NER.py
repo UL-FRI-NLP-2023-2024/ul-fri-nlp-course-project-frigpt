8ffed664d8f9b4383ae655dc5ac891e39696a619
JanRojc <jr3628@student.uni-lj.si>
2024-05-15 23:31:10 +0200

    refactor NER script

--- source/utils/NER.py
+++ source/utils/NER.py
@@ -16,8 +16,6 @@ def bert_LLM_entities(text, character):
 	# Load the NER pipeline with a pre-trained BERT model
 	ner_pipeline = pipeline("ner", model="dbmdz/bert-large-cased-finetuned-conll03-english")
 
-	print("Model loaded")
-
 	cnt = 0
 	# split the text into chunks of 2000 characters
 	for i in range(len(text)//2000):
@@ -36,11 +34,44 @@ def bert_LLM_entities(text, character):
 	print(f"Number of {character} entities:", cnt)
 
 
-def plot_NER_results(char_locs, x_axis, peaks, kde):
+def plot_NER_results(text, character, top_k=10):
+	'''
+		Plots the Named Entity Recognition (NER) results for given book and character.
+		Computes the Kernel Density Estimation (KDE) for the character locations.
+		Plots the KDE with different sigmas and the peaks in the KDE.
+	'''
+	# Perform NER on the text
+	char_locs, parsed_text = string_matching_NER(text, character)
 
-	get_character_locations(text, character, plot=True)
-	# TODO: plot the character locations and peaks in the KDE
-	return
+	##################################
+	# plot KDE with different sigmas #
+	for sigma in [100, 200, 300]:
+		kde, x_axis = get_KDE(char_locs, sigma=sigma, kernel_width=1000)
+		plt.plot(kde, label="sigma="+str(sigma))
+	plt.title("KDE with Gaussian Kernel")
+	plt.xlabel("Position in book")
+	plt.ylabel("Density")
+	plt.xticks(np.arange(0, len(kde), 5000), x_axis[::5000])
+	plt.legend()
+
+	#######################
+	# plot KDE with peaks #
+	kde, x_axis = get_KDE(char_locs, sigma=200, kernel_width=1000)
+	peaks, _ = find_peaks(kde, height=0.01)
+
+	# retain only top_k highest peaks
+	peaks = peaks[np.argsort(kde[peaks])[-top_k:]]
+
+	# plot peaks
+	plt.figure()
+	plt.plot(kde, label="KDE")
+	plt.vlines(peaks, 0, 0.08, color='r', linestyles='--', label="Peaks")
+	plt.title("Peaks in KDE")
+	plt.xlabel("Position in book")
+	plt.ylabel("Density")
+	plt.xticks(np.arange(0, len(kde), 5000), x_axis[::5000])
+	plt.legend()
+	plt.show()
 
 
 def get_gauss_kernel(sigma, kernel_width):
@@ -73,18 +104,18 @@ def get_KDE(char_locs, sigma, kernel_width):
 	return kde, x_axis
 
 
-def naive_NER(text, character):
+def string_matching_NER(text, character):
 	'''
-		Naive Named Entity Recognition (NER) using string matching.
+		Named Entity Recognition (NER) using string matching.
 		Searches for the character in the text and saves the indices where the character is found.
 		Text is preprocessed to remove unnecessary whitespaces and newlines.
 	'''
 
 	# convert character to lowercase
-	character = character.lower()
+	character = character.capitalize()
 
 	# remove unnecessary whitespaces and newlines
-	text = text.lower().replace("\n", " ").replace("\r", " ").replace("\t", " ")
+	text = text.replace("\n", " ").replace("\r", " ").replace("\t", " ")
 	text = " ".join(text.split())
 
 	# get character indices in text
@@ -96,20 +127,29 @@ def naive_NER(text, character):
 			char_locs.append(search_idx)
 			search_idx += 1
 
-	print(f"Number of {character} entities:", len(char_locs))
 	return np.array(char_locs), text
 
 
-def get_character_locations(text, character, sigma=200, kernel_width=1000, top_k=10, plot=False):
+def get_context(text, idx, context_size=1000):
+	'''
+		Returns the context around the given index in the text.
+	'''
+	# get the context around the index
+	context = text[idx-context_size//2:idx+context_size//2]
+	# remove the incomplete sentences
+	context = ".".join(context.split(".")[1:-1])
+	return context
+
+
+def get_character_locations(text, character, sigma=200, kernel_width=1000, top_k=10):
 	'''
 		Performs Named Entity Recognition (NER) on the text to find the character locations.
 		Computes the Kernel Density Estimation (KDE) to find the highest frequency locations of the character.
 		Returns the character locations, peak locations, and the parsed text.
 	'''
 
-
 	# Perform NER on the text
-	char_locs, parsed_text = naive_NER(text, character)
+	char_locs, parsed_text = string_matching_NER(text, character)
 
 	# get KDE with gaussian kernel
 	kde, x_axis = get_KDE(char_locs, sigma=sigma, kernel_width=kernel_width)
@@ -119,17 +159,6 @@ def get_character_locations(text, character, sigma=200, kernel_width=1000, top_k
 
 	# retain only top_k highest peaks
 	peaks = peaks[np.argsort(kde[peaks])[-top_k:]]
-
-	# plot peaks with vline
-	if plot:
-		plt.plot(kde, label="KDE")
-		plt.vlines(peaks, 0, 0.08, color='r', linestyles='--', label="Peaks")
-		plt.title("Peaks in KDE")
-		plt.xlabel("Position in book")
-		plt.ylabel("Density")
-		plt.xticks(np.arange(0, len(kde), 5000), x_axis[::5000])
-		plt.legend()
-		plt.show()
 
 	return char_locs, x_axis[peaks], parsed_text
 
@@ -144,13 +173,11 @@ if __name__ == "__main__":
 
 
 	# Perform NER with KDE computation to get the highest frequency locations of the character
-	all_locs, peak_locs, parsed_text = get_character_locations(text, character, plot=False)
-
+	all_locs, peak_locs, parsed_text = get_character_locations(text, character)
 
 	# print the text around the first peak
-	padding = 1000
-	text_block = parsed_text[peak_locs[0]-padding:peak_locs[0]+padding]
-	text_block = ".".join(text_block.split(".")[1:-1])
-	print(text_block)
+	context = get_context(parsed_text, peak_locs[0], context_size=2000)
+	print(context)
 
-
+	# # plot the NER results with KDE and peaks
+	# plot_NER_results(text, character)
