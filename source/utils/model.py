24502608f21363667015714af19feed2e39ab946
Leon Todorov <61196362+LeonTodorov@users.noreply.github.com>
2024-05-23 14:39:56 +0200

    Updating files

---
+++ source/utils/model.py
@@ -0,0 +1,30 @@
+# from google.colab import userdata
+from transformers import LlamaForCausalLM, LlamaTokenizer, pipeline, BitsAndBytesConfig
+import torch
+
+
+def create_text_generator():
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True, # loading in 4 bit
+        bnb_4bit_quant_type="nf4", # quantization type
+        bnb_4bit_use_double_quant=True, # nested quantization
+        bnb_4bit_compute_dtype=torch.bfloat16,
+    )
+
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = LlamaForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", token="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", torch_dtype=torch.float16, quantization_config=bnb_config)
+    tokenizer = LlamaTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf", token="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", torch_dtype=torch.float16)
+
+    text_generator = pipeline('text-generation',
+                            model=model,
+                            tokenizer=tokenizer,
+                            torch_dtype=torch.bfloat16,
+                            # device=0,
+                            # device_map="auto",
+                            do_sample=True,
+                            top_k=10,
+                            num_return_sequences=1,
+                            max_length=5_000,
+                            )
+    
+    return text_generator
